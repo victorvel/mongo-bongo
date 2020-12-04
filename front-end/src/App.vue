a9189a57fce5a1fb4a2dbe1300afac0c95f1c1fe
zking17-Student <70534914+zking17-Student@users.noreply.github.com>
2020-12-03 22:28:35 -0700

    yolo

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,30 +3,42 @@
     <div class="header">
       <router-link to="/">
         <div class="logo">
-          <img style="max-width: 180px;" src="../public/question.jpg" />
+          <img style="max-width: 180px" src="../public/question.jpg" />
         </div>
       </router-link>
       <div class="title">
         <h1>Q&A</h1>
-        <router-link to = "/" tag = button class = "navbar">Ask Questions</router-link>
-         <router-link to="/Answer" tag = button
-         class = "navbar">Answer Questions</router-link>
-         <router-link to="/Display" tag = button
-         class = "navbar">View Questions</router-link>
+        <div class="nav-container">
+          <router-link to="/" tag="button" class="navbar"
+            >Ask Questions</router-link
+          >
+          <router-link to="/Answer" tag="button" class="navbar"
+            >Answer Questions</router-link
+          >
+          <router-link to="/Display" tag="button" class="navbar"
+            >View Questions</router-link
+          >
+        </div>
       </div>
     </div>
     <div class="content">
       <router-view />
     </div>
     <div class="footer">
-      <br>
-          <a href="https://github.com/victorvel/mongo-bongo.git">
-            Github Classrom Link</a>
+      <br />
+      <a href="https://github.com/victorvel/mongo-bongo.git">
+        Github Classrom Link</a
+      >
     </div>
   </div>
 </template>
 
 <style>
+.nav-container {
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: space-around;
+}
 .navbar {
   margin: 10px 30px;
   margin-left: auto;
@@ -42,7 +54,7 @@
   margin: 5px 5px 5px 5px;
 }
 #app {
-  background-color: #2387E3;
+  background-color: #2387e3;
 }
 html {
   box-sizing: border-box;
@@ -51,7 +63,7 @@ html {
 body {
   font-family: "Montserrat", sans-serif;
   font-size: 16px;
-  background-color: #2387E3;
+  background-color: #2387e3;
   padding: 0px;
   margin: 0px;
 }
