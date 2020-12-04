f2f8d91544dfb654c9888da524e6cba6310e28c2
Victor Velasco <victorvelasco098@gmail.com>
2020-12-03 19:15:57 -0700

    initial set-up

---
+++ front-end/mongo-bongo/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <router-link to="/">
+        <div class="logo">
+          <img src="/ordinary.png" />
+        </div>
+      </router-link>
+      <div class="title">
+        <h1>Q&A</h1>
+      </div>
+    </div>
+    <div class="content">
+      <router-view />
+    </div>
+    <div class="footer">
+      <router-link to="/answer">Answer Qs</router-link>
+      <br>
+          <a href="https://github.com/victorvel/mongo-bongo.git">
+            Github Classrom Link</a>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header /
+.header {
+  display: flex;
+  padding: 10px 100px 0px 100px;
+  background-color: #5bdeff;
+  color: #1c454f;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.title h1 {
+  font-size: 30px;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+/ Footer */
+#app {
+  min-height: 90vh;
+}
+.footer {
+  height: 50px;
+  padding: 20px 100px 0px 100px;
+  background: #e3e3e3;
+  font-size: 12px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+.footer a {
+  color: #000;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 14px;
+}
+</style>
