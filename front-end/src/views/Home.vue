9a5e0e4d1a00ffda4c6e758e5ab186b4664956e7
zking17-Student <70534914+zking17-Student@users.noreply.github.com>
2020-12-03 21:45:30 -0700

    try again

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,86 +1,93 @@
 <template>
-    <div id="home">
-        <h1>Insert Question Here:</h1>
-        <div id="box">
-            <div id="input">
-                <input class="form" v-model="name" placeholder="Name">
-                <input class="form" v-model="title" placeholder="Title of post">
-                <textarea class="form" v-model="question" name="question" id="question" cols="30" rows="10" placeholder="Question..."></textarea>
-                <button @click="upload" style="width: 70px;">Submit</button>
-            </div>
-            <div class="upload" v-if="addItem">
-                <h2>{{addItem.name}}</h2>
-                <h3>{{addItem.title}}</h3>
-                <p>{{addItem.question}}</p>
-            </div>
-        </div>
+  <div id="home">
+    <h1>Insert Question Here:</h1>
+    <div id="box">
+      <div id="input">
+        <input class="form" v-model="name" placeholder="Name" />
+        <input class="form" v-model="title" placeholder="Title of post" />
+        <textarea
+          class="form"
+          v-model="question"
+          name="question"
+          id="question"
+          cols="30"
+          rows="10"
+          placeholder="Question..."
+        ></textarea>
+        <button @click="upload" style="width: 70px">Submit</button>
+      </div>
+      <div class="upload" v-if="addItem">
+        <h2>{{ addItem.name }}</h2>
+        <h3>{{ addItem.title }}</h3>
+        <p>{{ addItem.question }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    data() {
+  data() {
     return {
       name: "",
       question: "",
       title: "",
       addItem: null,
       items: [],
-      findName: ""
-    }
+      findName: "",
+    };
   },
   computed: {},
-    created() {
-        this.getItems();
-    },
+  created() {
+    this.getItems();
+  },
   methods: {
     async getItems() {
-    try {
+      try {
         let response = await axios.get("/api/items");
         this.items = response.data;
         return true;
-    } catch (error) {
+      } catch (error) {
         console.log(error);
-    }
-  },
-  async upload() {
+      }
+    },
+    async upload() {
       try {
-        let r1 = await axios.post('/api/items', {
+        let r1 = await axios.post("/api/items", {
           name: this.name,
           title: this.title,
-          question: this.question
+          question: this.question,
         });
         this.addItem = r1.data;
       } catch (error) {
         console.log(error);
       }
     },
-}
-}
-
+  },
+};
 </script>
 
 <style scoped>
 h1 {
-    text-align: center;
+  text-align: center;
 }
 .form {
-    background-color: lightgray;
+  background-color: lightgray;
 }
 #box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 #input {
-    display: flex; 
-    flex-direction: column;
-    width: 50%;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
 }
 #input input {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 </style>
